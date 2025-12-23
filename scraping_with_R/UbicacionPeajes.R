1731f859720e0d81531c3308763b5f289823e6b8
David Camelo <NICOLAS.CAMELO28@outlook.com>
2025-12-23 18:05:17 -0500

    Selecting specific columns

--- scraping_with_R/UbicacionPeajes.R
+++ scraping_with_R/UbicacionPeajes.R
@@ -1,20 +1,39 @@
-# Librerías necesarias
+# Required Libraries
 library(httr)
 library(jsonlite)
 library(dplyr)
 library(lubridate)
 library(purrr)
+library(tidyr)
 
-# URL base (nuevo recurso)
+# URL base
 url <- "https://www.datos.gov.co/resource/68qj-5xux.json"
 
 res <- GET(url)
 
 
-# Parsear JSON
+# JSON
 ubication_data <- fromJSON(
   content(res, "text", encoding = "UTF-8"),
   flatten = TRUE
 )
 
-ubication_data
+ubication_data <- ubication_data |>
+  select(
+    c_digo_peaje,
+    nombre_peaje,
+    ubicaci_n,
+    sector,
+    sentido,
+    url_foto.url,
+    responsable,
+    territorial,
+    poste_de_referencia_pr,
+    distancia_pr,
+    point.coordinates
+  ) |>
+  mutate(
+    longitud = map_dbl(point.coordinates, ~ .x[1]),
+    latitud = map_dbl(point.coordinates, ~ .x[2])
+  ) |>
+  select(-point.coordinates)
